77f1c4b7c03afca4933f9297735d8c35deceea45
Byron <byroncorrales@gmail.com>
2011-04-06 09:06:08 -0600

    publicaciones dinamicas

--- paginas/views.py
+++ paginas/views.py
@@ -6,21 +6,24 @@ from django.shortcuts import get_list_or_404
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.template.defaultfilters import slugify
+#from django.template.defaultfilters import slugify
 from django.views.generic.simple import direct_to_template
-from addac.noticias.models import *
-from addac.paginas.models import *
+from addac.noticias.models import Noticia
+from addac.paginas.models import Pagina
 from addac.banners.models import Banner
+from addac.publicaciones.models import Publicacion
 from tagging.models import *
 
 def inicio(request):
     noticia = Noticia.objects.filter(tipo = 1).order_by('-fecha', '-id')[:4]
     noticia_inov = Noticia.objects.filter(tipo = 2).order_by('-fecha', '-id')[:1]
     banner = Banner.objects.all()
+    publicacion = Publicacion.objects.filter(cidoc = True).order_by('-fecha', '-id')[:9]
     dicc = {
         'noticia':noticia,
         'noticia_inov':noticia_inov,
         'banner':banner,
+        'publicacion':publicacion,
     }
     return render_to_response('index.html',dicc,
                               context_instance=RequestContext(request))
