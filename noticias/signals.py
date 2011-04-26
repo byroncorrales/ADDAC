0999df3fb40d7dc36078e9283bc2302e9f07df42
Byron <byroncorrales@gmail.com>
2011-04-26 10:13:59 -0600

    notificaciones 7

--- noticias/signals.py
+++ noticias/signals.py
@@ -1,18 +1,14 @@
 # signals.py
 from django.contrib.sites.models import Site
 from django.template.loader import render_to_string
-from django.core.mail import mail_managers
-
+from django.core.mail import send_mail
 def comment_notifier(sender, comment, **kwargs):
 
     """ Email admins when a new comment is posted """
-
-    if not comment.is_public:
-        subject = "New comment by %s on %s" % (
-            comment.user_name,
-            Site.objects.get_current().domain)
-        body = render_to_string(
-            "email.txt", {
-                'comment': comment})
-        mail_managers(subject, body, fail_silently=False, connection=None)
-        send_mail(subject, body, 'no-reply@addac.org.ni', ['byroncorrales@gmail.com'])
+    subject = "New comment by %s on %s" % (
+        comment.user_name,
+        Site.objects.get_current().domain)
+    body = render_to_string(
+        "email.txt", {
+            'comment': comment})
+    send_mail(subject, body, 'no-reply@addac.org.ni', ['byroncorrales@gmail.com'])
