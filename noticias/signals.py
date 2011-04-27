edf31886aa7a73bb40a10c57dd452ee39727d1c8
Byron <byroncorrales@gmail.com>
2011-04-27 16:24:47 -0600

    404

--- noticias/signals.py
+++ noticias/signals.py
@@ -5,4 +5,4 @@ from django.core.mail import send_mail
 def comment_notifier(sender, **kwargs):
     """ Email admins when a new comment is posted """
     body = kwargs['instance'].name + ", comento" + ": " + kwargs['instance'].comment
-    send_mail("Nuevo Comentario en Sitio ADDAC", body, 'no-reply@addac.org.ni', ['byroncorrales@gmail.com','central@addac.org.ni'])
+    send_mail("Nuevo Comentario en Sitio ADDAC", body, 'no-reply@addac.org.ni', ['byroncorrales@gmail.com','antorcha@addac.org.ni'])
